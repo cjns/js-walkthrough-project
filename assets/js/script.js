bddf671d3acf7757fd9b9dc7fa28a0b2c2a81582
cjns <67065839+cjns@users.noreply.github.com>
2023-07-12 19:09:21 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+//  runGame()
+//  checkAnswer()
+//  calculateCorrectAnswer()
+//  incrementScore()
+//  incrementWrongAnswer()
+//  displayAdditionQuestion()
+//  displaySubtractQuestion()
+//  displayMultiplyQuestion()
+
+// runGame()
+function runGame() { }
+
+//  checkAnswer()
+function checkAnswer() { }
+
+//  calculateCorrectAnswer()
+function calculateCorrectAnswer() { }
+
+//  incrementScore()
+function incrementScore() { }
+
+//  incrementWrongAnswer()
+function incrementWrongAnswer() { }
+
+//  displayAdditionQuestions()
+function displayAdditionQuestion() { }
+
+//  displaySubtractQuestion()
+function displaySubtractQuestion() { }
+
+//  displayMultiplyQuestion()
+function displayMultiplyQuestion() { }
